730b751e2544c7e2750e91b3c1a562ed2e7a84dd
MkDierz <msyibbran24@gmail.com>
2021-09-24 11:34:50 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php ?>
+<table>
+    <tr>
+        <th>No</th>
+        <th>id</th>
+        <th>nama</th>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>1</td>
+        <td>resti</td>
+    </tr>
+</table>
